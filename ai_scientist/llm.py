00b66cb94bfd487906940f832df399202b5ca28f
jiminliang <jiminliang@gmail.com>
2025-02-06 16:11:20 +0800

    add chatgpt models

--- ai_scientist/llm.py
+++ ai_scientist/llm.py
@@ -15,8 +15,11 @@ AVAILABLE_LLMS = [
     "claude-3-5-sonnet-20240620",
     "claude-3-5-sonnet-20241022",
     # OpenAI models
+    "gpt-4o-mini",          # = gpt-4o-mini-2024-07-18
     "gpt-4o-mini-2024-07-18",
     "gpt-4o-2024-05-13",
+    "gpt-4o",               # = gpt-4o-2024-08-06
+    "gpt-4o-2024-11-20"
     "gpt-4o-2024-08-06",
     "o1-preview-2024-09-12",
     "o1-mini-2024-09-12",
